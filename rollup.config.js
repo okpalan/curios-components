5d4537c1e1982f53468220fa19d693d591266f79
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-09-19 23:00:47 +0100

    added-docs

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import terser from '@rollup/plugin-terser'; // Import the Terser plugin correctly
+import terser from '@rollup/plugin-terser'; 
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
--- scripts/manage-dependencies.sh
+++ scripts/manage-dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Check for pnpm
-if command -v pnpm &> /dev/null
-then
+if command -v pnpm &> /dev/null; then
     PACKAGE_MANAGER="pnpm"
 else
     PACKAGE_MANAGER="npm"
 fi
+
 # Run the build command
 $PACKAGE_MANAGER run build
 
