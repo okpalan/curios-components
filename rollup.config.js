d7f982320b4a25d3acfe7be7f5ebd22584e3c257
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-09-19 22:53:35 +0100

    added-docs

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,11 @@ import path from 'path';
 import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import umd from 'rollup-plugin-umd';
 
 const inputDir = 'src/';
 const outputDir = 'dist';
+const umdDir = path.join(outputDir, 'umd/draft-components');
 
 // List all JavaScript files in the src directory
 const components = fs.readdirSync(inputDir).filter(file => file.endsWith('.js') || file === 'index.js');
@@ -13,26 +15,35 @@ const components = fs.readdirSync(inputDir).filter(file => file.endsWith('.js') 
 console.log('Components:', components); // Debugging line
 
 // Create a Rollup config for each component
-const createConfig = (component) => ({
-  input: path.join(inputDir, component),
-  output: [
-    {
-      file: path.join(outputDir, component.replace('.js', '.cjs.js')),
-      format: 'cjs',
-      sourcemap: true
-    },
-    {
-      file: path.join(outputDir, component.replace('.js', '.esm.js')),
-      format: 'esm',
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    resolve(),
-    commonjs(),
-    terser()
-  ]
-});
+const createConfig = (component) => ([
+  {
+    input: path.join(inputDir, component),
+    output: [
+      {
+        file: path.join(outputDir, component.replace('.js', '.cjs.js')),
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: path.join(outputDir, component.replace('.js', '.esm.js')),
+        format: 'esm',
+        sourcemap: true
+      },
+      {
+        file: path.join(umdDir, component.replace('.js', '.umd.js')),
+        format: 'umd',
+        name: 'DraftComponents', // Adjust the name as necessary
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      terser(),
+      umd() // Include UMD plugin
+    ]
+  }
+]);
 
 // Export an array of configurations
-export default components.map(createConfig);
+export default components.flatMap(createConfig);
