acaf8dd46af82aed15a0cb6dc79d80cb2133e7e6
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-09-20 00:30:18 +0100

    a

--- test-runner.config.mjs
+++ test-runner.config.mjs
@@ -1,20 +1,18 @@
-
-import { createRunner } from '@web/test-runner';
 import { html, fixture } from '@open-wc/testing';
 
 export default {
   // Use 'chrome' or 'firefox' for browser-based testing
   browsers: ['chrome'],
   files: './tests/**/*.test.js',
+  // Set to 'true' to see browser console output
+  verbose: false,
+  // Set to 'true' to see browser errors
+  debug: false,
+  
   nodeResolve: true,
+  testIsolation: false,
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   plugins: [
-    createRunner({
-      config: {
-        testEnvironment: 'jsdom',
-        transform: {},
-        setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-      },
-    }),
     {
       name: 'test-setup',
       setup() {
@@ -22,6 +20,6 @@ export default {
         globalThis.fixture = fixture;
       },
     },
+    
   ],
-  testIsolation: false,
 };
