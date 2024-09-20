69c2f3c7c3a25131d25ce8c1b8fbb4d85067d6f2
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-09-20 04:04:33 +0100

    a

--- tests/unit/noisy-notification.test.js
+++ tests/unit/noisy-notification.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable-next-line import/no-unused-modules */
-import NoisyNotification from '../../../src/main.js';
+import NoisyNotification from '../../src/main.jd';
 
 describe('NoisyNotification', () => {
     let element;
