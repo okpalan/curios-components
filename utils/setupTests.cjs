a214076e38c0134855cb6de0fb672e28b403f2dd
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-09-20 01:40:33 +0100

    karm

--- utils/setupTests.cjs
+++ utils/setupTests.cjs
@@ -1,15 +1,16 @@
 require('@testing-library/jest-dom');
 
 
-const { JSDOM } = require('jsdom');
-const { html, fixture } = require('@open-wc/testing');
-
-
 // Polyfill TextEncoder and TextDecoder
 const { TextEncoder, TextDecoder } = require('util');
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
+const { JSDOM } = require('jsdom');
+const { html, fixture } = require('@open-wc/testing');
+
+
 // Create a jsdom environment
 const { window } = new JSDOM();
 globalThis.window = window;
