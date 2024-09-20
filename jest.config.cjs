1987ceb71bcd4286bc10cd3f9608c0256a3aa5a3
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-09-20 04:38:36 +0100

    a

--- jest.config.cjs
+++ jest.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
   collectCoverageFrom: [
     "src/**/*.{js,jsx,ts,tsx}",
     "!**/node_modules/**",
-  ]
+  ],
   transform: {
     '^.+\\.jsx?$': ['babel-jest', { configFile: './babel.config.test.cjs' }],
     '^.+\\.ts$': 'ts-jest',
