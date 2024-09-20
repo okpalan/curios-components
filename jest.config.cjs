a0b18e4dff9791277d818e10f682d6f9fe931296
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-09-20 02:12:01 +0100

    karm

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,11 +2,14 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./utils/setupTests.mjs'],
+  extensionsToTreatAsEsm: ['.ts', '.tsx', '.mjs'], 
+
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    "<rootDir>/node_modules/(?!(your-esm-module|another-esm-module)/)" // If you have specific ESM modules to transform
-  ],
+    'node_modules/(?!(@open-wc)/)',
+
+   ],
 };
