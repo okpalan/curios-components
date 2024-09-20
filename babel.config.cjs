932aa1bc46b7934c82d56699b798058e6a1b2ed2
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-09-20 02:44:21 +0100

    a

--- babel.config.cjs
+++ babel.config.cjs
@@ -13,7 +13,7 @@ module.exports = {
       plugins: [
         "@babel/plugin-transform-modules-commonjs" ,
         babel({
-          babelHelpers: 'bundled',
+          babelHelpers: 'runtime', 
           exclude: 'node_modules/**',
         }),
       ]
