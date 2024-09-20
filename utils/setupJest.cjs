a3556bb620024c06674291759e8a580a2c4b29e6
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-09-20 04:13:55 +0100

    a

--- utils/setupJest.cjs
+++ utils/setupJest.cjs
@@ -1,15 +1,8 @@
-// utils/setupJest.js
 
-// Example Jest setup configuration
-module.exports = function setupJest() {
-    // Configure global variables or mocks here
-    global.someGlobalVariable = 'value';
-  
-    // Setup any required modules or mock functions
-    jest.mock('some-module', () => ({
-      someFunction: jest.fn(() => 'mocked value'),
-    }));
-  
+function setupJest() {
+    // Your setup code here
     console.log("Jest setup complete!");
-  };
+  }
   
+  
+  module.exports = setupJest;
