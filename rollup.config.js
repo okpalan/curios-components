5e1d8df88088dea9eb9ff5fa531d8d49db08b3d8
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-09-20 13:51:21 +0100

    a

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import glob from 'glob';
+import {glob} from 'glob';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
