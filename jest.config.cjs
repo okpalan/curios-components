9d281202e99e469076336044cc87190b74c0b7eb
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-09-20 02:36:23 +0100

    a

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,17 +1,14 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./utils/setupTests.cjs'],
-  extensionsToTreatAsEsm: ['.ts', '.tsx',], 
+  extensionsToTreatAsEsm: ['.ts', '.tsx'], 
 
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
   transform: {
-    transform: {
-      '^.+\\.[jt]sx?$': 'babel-jest',
-    },
-      },
+    '^.+\\.[jt]sx?$': 'babel-jest',  // Removed redundant "transform" key
+  },
   transformIgnorePatterns: [
-    '/node_modules/',
+    '/node_modules/',  // This is fine if you want to ignore all node modules
     '^.+\\.module\\.(css|sass|scss)$',
-    
-   ],
+  ],
 };
--- utils/setupTests.cjs
+++ utils/setupTests.cjs
@@ -1,20 +1,18 @@
 (async () => {
-    const { html, fixture } = await import('@open-wc/testing');
-    const { JSDOM } = await import('jsdom');
-    const { TextEncoder, TextDecoder } = await import('util');
-  
-    // Polyfill TextEncoder and TextDecoder
+    const { html, fixture } = require('@open-wc/testing');
+    const { JSDOM } = require('jsdom');
+    const { TextEncoder, TextDecoder } = require('util');
+    
     global.TextEncoder = TextEncoder;
     global.TextDecoder = TextDecoder;
-  
-    // Create a jsdom environment
+    
     const { window } = new JSDOM();
     globalThis.window = window;
     globalThis.document = window.document;
     globalThis.navigator = window.navigator;
-  
-    // Set up test helpers
+    
     globalThis.html = html;
     globalThis.fixture = fixture;
+    
   })();
   
