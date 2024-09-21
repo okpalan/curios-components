f9b044a432b66f131d973b1a87511283343f58ae
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-09-21 21:04:59 +0100

    a

--- src/main.cjs
+++ src/main.cjs
@@ -1,5 +1,5 @@
-const setupJest = require('../utils/setupJest.cjs'); // Ensure the path is correct
-const setupTests = require('../utils/setupTests.cjs'); // Adjust if necessary
+const setupJest = require('../utils/jest.setup.cjs/index.js'); // Ensure the path is correct
+const setupTests = require('../utils/tests.setup.cjs/index.js'); // Adjust if necessary
 
 setupJest(); // Call the function
 setupTests(); // Call the function
